bc70ff943d0a3f20bd907f2f0cba95c5c46e4ba9
Isah Ibrahim <isahibn08@gmail.com>
2023-07-03 12:02:38 +0100

    added some necessary data for the contents

---
+++ src/utils/contents.ts
@@ -0,0 +1,269 @@
+export const Introduction = {
+	id: '6276a731d480db4e54fb1d7b',
+	title: 'Financial Education',
+	intro:
+		"Financial literacy is a crucial aspect that should be introduced to children at a young age. As a parent, it's important to teach your children about the significance of financial freedom and the importance of making wise financial decisions.",
+	content: {
+		card: [
+			{
+				sn: 1,
+				text: "This book is specifically tailored for children aged between 7 and 13. It aims to equip them with essential financial knowledge. In this technologically advanced generation, it's crucial to provide them with the necessary education to handle money effectively. It is important for every child to develop an abundance mindset rather than a scarcity mindset.",
+			},
+		],
+		resources: ['url-'],
+	},
+};
+
+export const Savings = {
+	id: '6278784aa79ad028b00d80fc',
+	title: 'Building Savings',
+	intro:
+		"Savings can be defined as the practice of setting aside money for future needs. Instead of spending all your income, it's important to prioritize saving for future expenses or unexpected situations.",
+	content: {
+		card: [
+			{
+				sn: 1,
+				text: 'As Warren Buffet wisely said, "Don\'t save what is left after spending; spend what is left after saving."',
+			},
+			{
+				sn: 2,
+				text: "Developing good saving habits is essential for establishing a strong financial foundation. It's never too early to start saving, even as a young person. You can choose to keep your savings in a piggy bank or deposit them in a bank account.",
+			},
+			{
+				sn: 3,
+				title: 'Benefits of Saving',
+				list: [
+					'Building funds for purchasing your first home',
+					'Investing in a business venture',
+					'Saving for a car or vehicle purchase',
+					'Accumulating funds for acquiring land or property',
+					'Creating a passive income stream through rental properties',
+					'Funding your travel aspirations',
+				],
+			},
+			{
+				sn: 4,
+				text: 'Saving is a fundamental skill that every young person should cultivate. Learning how to save effectively can help you become a responsible and financially wise individual. It means learning to be mindful of your spending and avoiding unnecessary expenses.',
+			},
+			{
+				sn: 5,
+				text: 'You can keep your savings in various financial institutions such as banks, credit unions, or brokerage firms. These institutions provide a safe place to store your money until you need it.',
+			},
+		],
+		resources: ['url-'],
+	},
+};
+
+export const Budgeting = {
+	id: '62785c08a79ad028b00d80fb',
+	title: 'Financial Planning',
+	intro:
+		'Hey there, young learner! Get ready to explore the world of personal finance and discover how the concepts below can shape your financial journey. By the end of this book, you will have a solid grasp of managing your money, saving effectively, and the art of smart investments.',
+	content: {
+		card: [
+			{
+				sn: 1,
+				text: "Let's start our learning adventure with financial planning. But what exactly is financial planning?",
+			},
+			{
+				sn: 2,
+				text: 'Financial planning involves creating a comprehensive list of items you intend to purchase. It is crucial to consider your income while crafting a financial plan. The primary objective of financial planning is to ensure that your income is wisely allocated to cover your expenses. Your plan should prioritize your essential needs.',
+			},
+			{
+				sn: 3,
+				title: 'Why Is Financial Planning Important?',
+				text: 'Engaging in financial planning empowers you to organize your money, enabling you to afford both your desires and necessities. Financial planning can:',
+				list: [
+					'Help you assess whether you have sufficient funds for your needs and wants',
+					'Provide clarity on how you allocate your funds',
+					'Help you distinguish between essential and non-essential expenses',
+					'Enable you to save for important yet currently unaffordable goals',
+					'Assist in minimizing or eliminating debt',
+					'Relieve financial stress by ensuring stability',
+					'Provide an overview of your financial status and spending patterns',
+				],
+			},
+			{
+				sn: 4,
+				title: 'How Can You Create Your Own Financial Plan?',
+				list: [
+					'Establish clear financial goals',
+					'Compile a list of items you need to purchase and arrange them by priority',
+					'Distinguish between wants and needs',
+					'Customize your financial plan to align with your goals and needs',
+					'Put your plan into action!',
+				],
+			},
+			{
+				sn: 5,
+				title: 'What Does a Financial Plan Look Like?',
+				text: 'Here is an example of a sample financial plan for monthly expenses/bills.',
+				table: [
+					{ Category: 'Housing', Cost: '$900' },
+					{ Category: 'Transportation', Cost: '$150' },
+					{ Category: 'Groceries', Cost: '$200' },
+					{ Category: 'Utilities', Cost: '$100' },
+					{ Category: 'Entertainment', Cost: '$50' },
+					{ Category: 'Miscellaneous', Cost: '$100' },
+				],
+			},
+			{
+				sn: 6,
+				title: 'Scenario',
+				text: "Meet Daniel, a recent college graduate and a full-time employee at Fountain View Corporation. Daniel receives a paycheck twice a month, totaling $2400 per month. Let's explore Daniel's monthly budget based on the provided information. Calculate Daniel's monthly expenses and determine the remaining amount after paying bills.",
+				table: [
+					{ Category: 'Rent', Cost: '$700' },
+					{ Category: 'Utilities', Cost: '$100' },
+					{ Category: 'Groceries', Cost: '$150' },
+					{ Category: 'Transportation', Cost: '$80' },
+					{ Category: 'Entertainment', Cost: '$50' },
+					{ Category: 'Miscellaneous', Cost: '$70' },
+				],
+			},
+		],
+		resources: ['url-'],
+	},
+};
+
+export const Debt = {
+	id: '62885077b25e726df6e4e4d2',
+	title: 'Managing Debt',
+	intro:
+		'Debt refers to the amount of money that you owe to individuals, businesses, or the government. It is important to understand and manage your debt responsibly.',
+	content: {
+		card: [
+			{
+				sn: 1,
+				text: 'While it may not always be possible to completely avoid debt, it is crucial to exercise control and effective management. As you strive towards financial success, it is important to manage your finances wisely and minimize your reliance on debt.',
+			},
+		],
+	},
+};
+
+export const Spending = {
+	id: '62885423b25e726df6e4e4d3',
+	title: 'Smart Spending',
+	intro:
+		'Spending money refers to the funds that you possess or receive for personal expenses. It involves exchanging money for goods or services.',
+	content: {
+		card: [
+			{
+				sn: 1,
+				title: 'Making Wise Spending Choices',
+				text: 'While there will always be various tempting things to spend your money on, it is important to develop smart spending habits. Avoid spending your money on unnecessary items and focus on managing your finances wisely. Make thoughtful choices when it comes to spending your hard-earned money.',
+			},
+			{
+				sn: 2,
+				title: 'Tips for Wise Spending',
+				list: [
+					'Avoid impulse purchases and only buy what you truly need.',
+					'Create a budget to allocate your funds effectively.',
+					'Strive to adhere to your budget and avoid overspending.',
+					'Practice the habit of saving a portion of your income.',
+					'Don’t give in to every desire to make a purchase; prioritize your needs.',
+				],
+			},
+		],
+	},
+};
+
+export const Income = {
+	id: '6288751db25e726df6e4e4d5',
+	title: 'Understanding Income',
+	intro:
+		'Income refers to the money you regularly receive, either from working or through investments.',
+	content: {
+		card: [
+			{
+				sn: 1,
+				text: 'For example, if John earns $450 per month by working at McDonald’s, we can say that his monthly income is $450.',
+			},
+			{
+				sn: 2,
+				list: [
+					'Here’s a helpful trick to remember: income is the money that comes to you.',
+					"As a young individual, it's important to understand that money doesn’t appear magically. Remember that your parents' spending money is earned through their work. That is their income.",
+				],
+			},
+			{
+				sn: 3,
+				title: 'Managing Your Income',
+				text: 'Financially responsible individuals do not spend all of their income. They allocate a portion for savings and differentiate between their needs and wants to make informed spending decisions.',
+			},
+			{
+				sn: 4,
+				title: 'Distinguishing Between Needs and Wants',
+				text: "Have you ever asked your parents for something you wanted, but they refused? It's likely because they believed it wasn’t a necessary item at that time.",
+			},
+			{
+				sn: 5,
+				text: 'Needs are essential items that should be prioritized in your budget. For example, food is a necessity for survival, while new clothes may be something you want but don’t necessarily need. Understanding the difference between needs and wants will help you save money and develop good financial habits.',
+			},
+		],
+	},
+};
+
+export const Investing = {
+	id: '6288793cb25e726df6e4e4d7',
+	title: 'Investing Basics',
+	intro:
+		'If you aspire to become a millionaire one day, it’s crucial to understand the concept of investing, how it works, and how you can invest your own money.',
+	content: {
+		card: [
+			{
+				sn: 1,
+				title: 'What is Investing?',
+				text: "Investing involves putting your money into different financial instruments to generate additional income through compound interest. The earlier you start investing, the more you can potentially earn. Investing is all about timing, so it's important to begin at a young age.",
+			},
+			{
+				sn: 2,
+				title: 'Types of Investments',
+				list: [
+					'Stocks',
+					'Mutual Funds',
+					'Index Funds',
+					'Money Market',
+					'Certificates of Deposit (CD)',
+				],
+			},
+			{
+				sn: 3,
+				title: 'Stocks',
+				text: 'Stocks represent ownership in a company. When you buy a company’s stock, it means you own a small part of that company. Companies sell stock to raise money for purchasing necessary items or materials.',
+			},
+			{
+				sn: 4,
+				title: 'Why Do People Buy Stocks?',
+				text: 'Most people buy stocks with the goal of making a profit. When a company generates profit and its stock value increases, investors receive dividends or returns on their investment.',
+			},
+		],
+	},
+};
+
+export const Conclusion = {
+	id: '628aab80b25e726df6e4e4e1',
+	title: 'Conclusion',
+	intro:
+		'FinKid, I’m thrilled that you have acquired the fundamentals of finance at such a young age. You are on your way to becoming an adult who knows how to earn money and grow it further. You will also understand the importance of saving and make informed decisions about where to save your money. Best of luck on your journey to financial freedom and literacy!',
+	content: {
+		card: [
+			{
+				sn: 1,
+				text: "Many people overlook the importance of insurance, particularly life insurance. Life insurance provides coverage for a person's life. When someone has life insurance, their family or a designated beneficiary receives a sum of money after their death.",
+			},
+			{
+				sn: 2,
+				text: 'Life insurance holds significant value as it can help protect families and friends from unexpected expenses, such as funeral costs.',
+			},
+			{
+				sn: 3,
+				text: 'Life insurance can:',
+				list: [
+					'Assist your family in dealing with unexpected expenses',
+					'Aid in paying off any outstanding debts',
+					'Support your family when you are no longer around',
+				],
+			},
+		],
+	},
+};
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,199 +1,213 @@
 export const juniorData = [
-     {
-    id: 1,
-    page: 1,
-    title: "5 Naira Note",
-    image: "",
-    text: "Sir Abubakar Tafawa Balewa, Nigeria's first Prime Minister, is on the front of the 5 naira note. The back side of the five naira banknote shows traditional yoruba drummers. Also shown on the back side is a map outline of Nigeria with the Nigerian flag, green white green in vertical bands.",
-    exercise: [
-      {
-        id: 1,
-        question: "Count and add up all the 5 Naira note(s).",
-        type: '5naira',
-        objectives: [{
-          answer: 1,
-          options: [2, 3, 1, 4]
-        }, {
-          answer: 3,
-          options: [5, 3, 6, 4]
-        }, {
-          answer: 2,
-          options: [2, 8, 6, 4]
-        },]
-        // options: [1, 3, 2],
-      },
-    ],
-  },
-  {
-    id: 2,
-    page: 2,
-    title: "10 Naira Note",
-    image: "",
-    text: "Alvan Ikoku, a Nigerian educationist and statesman, is featured on the front of the 10 Naira note in Nigeria.The back side of the ten naira note is two women with baskets on heads which also represents the cultural heritage of Nigeria.",
-    exercise: [
-      {
-        id: 1,
-        question: "Count and add up all the 10 naira note(s).",
-        type: '10naira',
-        objectives: [{
-          answer: 1,
-          options: [2, 3, 1, 4]
-        }, {
-          answer: 4,
-          options: [5, 3, 6, 4]
-        }, {
-          answer: 3,
-          options: [2, 3, 6, 4]
-        },]
-        // options: [1, 3, 2],
-      },
-    ],
-  },
-  {
-    id: 3,
-    page: 3,
-    title: "20 Naira Note",
-    image: "",
-    text: "The green-coloured paper banknote of Twenty Naira features the portrait of Army General Murtala R. Muhammed (1938-1976) in military uniform on the front side. The back side of the old ₦20 bill shows the coat of arms of Nigeria with the national motto “Unity and Faith”.",
-    exercise: [
-      {
-        id: 1,
-        question: "Count and add up all the 20 naira note(s).",
-        type: '20naira',
-        objectives: [{
-          answer: 3,
-          options: [2,3,1,4]
-        }, {
-            answer: 2,
-            options: [5, 3, 6, 2]
-          }, {
-            answer: 4,
-            options: [2, 8, 6, 4]
-          },]
-        // options: [1, 3, 2],
-      },
-    ],
-  },
-  {
-    id: 4,
-    page: 4,
-    title: "50 Naira Note",
-    text: "The blue-coloured, polymer banknote of 50 Naira features the People of Nigeria, showing four portraits of Nigerians. The ₦50 bill has a see-through window. On the back side of the Fifty Naira banknote is an image of three fishermen at work.",
-    image: "",
-    exercise: [
-      {
-        id: 1,
-        question: "Count and add up all of the 50 naira note.",
-        type: '50naira',
-        objectives: [{
-          answer: 3,
-          options: [2, 3, 1, 4]
-        }, {
-          answer: 4,
-          options: [4, 3, 6, 2]
-        }, {
-          answer: 1,
-          options: [2, 8, 6, 1]
-        },]
-        // options: [1, 3, 2],
-      },
-    ],
-  },
-  {
-    id: 5,
-    page: 5,
-    title: "Drag and Drop",
-    text: "Identify the Naira Notes below by dragging and dropping each note on the right place.",
-    image: "",
-    exercise: [
-      {
-        id: 1,
-        question: "Drag coin from the left to its value on the right",
-        options: [
-          { image: "five.png", name: "Five" },
-          { image: "ten.png", name: "Ten" },
-          { image: "twenty.png", name: "Twenty" },
-          { image: "fifty.png", name: "Fifty" },
-        ],
-      },
-    ],
-  },
-  {
-    id: 6,
-    page: 6,
-    title: "Drag and Drop",
-    text: "Identify the Naira Notes below by dragging and dropping each note on the right place.",
-    image: "",
-    exercise: [
-      {
-        id: 1,
-        question: "Drag coin from the left to its value on the right",
-        options: [
-          { image: "five.png", name: "Five" },
-          { image: "ten.png", name: "Ten" },
-          { image: "twenty.png", name: "Twenty" },
-          { image: "fifty.png", name: "Fifty" },
-        ],
-      },
-    ],
-  },
-  {
-    id: 7,
-    page: 7,
-    title: "Drag and Drop",
-    text: "Identify the Naira Notes below by dragging and dropping each note on the right place.",
-    image: "",
-    exercise: [
-      {
-        id: 1,
-        question: "Drag coin from the left to its value on the right",
-        options: [
-          { image: "five.png", name: "Five" },
-          { image: "ten.png", name: "Ten" },
-          { image: "twenty.png", name: "Twenty" },
-          { image: "fifty.png", name: "Fifty" },
-        ],
-      },
-    ],
-  },
-  {
-    id: 8,
-    page: 8,
-    title: "Let's Shop",
-    text: "pick the coins below to show how much you need to buy this little bunny.",
-    image: "",
-    exercise: [
-      {
-        id: 1,
-        options: [],
-        answer: "25 Naira",
-      },
-    ],
-  },
-  {
-    id: 9,
-    page: 9,
-    title: "Making Lemonade!",
-    text: "Lulu wants a new game. It costs 250 Naira. To earn money Lulu has a lemonade stand. Each glass of lemonade costs 5 Naira. Tell how many glass lulu needs to sell to buy the game. (Hint: 250 divided by 5)",
-    image: "",
-    exercise: [
-      {
-        id: 1,
-        options: [],
-      },
-    ],
-  },
-]
-
+	{
+		id: 1,
+		page: 1,
+		title: '5 Naira Note',
+		image: '',
+		text: "Sir Abubakar Tafawa Balewa, Nigeria's first Prime Minister, is on the front of the 5 naira note. The back side of the five naira banknote shows traditional yoruba drummers. Also shown on the back side is a map outline of Nigeria with the Nigerian flag, green white green in vertical bands.",
+		exercise: [
+			{
+				id: 1,
+				question: 'Count and add up all the 5 Naira note(s).',
+				type: '5naira',
+				objectives: [
+					{
+						answer: 1,
+						options: [2, 3, 1, 4],
+					},
+					{
+						answer: 3,
+						options: [5, 3, 6, 4],
+					},
+					{
+						answer: 2,
+						options: [2, 8, 6, 4],
+					},
+				],
+				// options: [1, 3, 2],
+			},
+		],
+	},
+	{
+		id: 2,
+		page: 2,
+		title: '10 Naira Note',
+		image: '',
+		text: 'Alvan Ikoku, a Nigerian educationist and statesman, is featured on the front of the 10 Naira note in Nigeria.The back side of the ten naira note is two women with baskets on heads which also represents the cultural heritage of Nigeria.',
+		exercise: [
+			{
+				id: 1,
+				question: 'Count and add up all the 10 naira note(s).',
+				type: '10naira',
+				objectives: [
+					{
+						answer: 1,
+						options: [2, 3, 1, 4],
+					},
+					{
+						answer: 4,
+						options: [5, 3, 6, 4],
+					},
+					{
+						answer: 3,
+						options: [2, 3, 6, 4],
+					},
+				],
+				// options: [1, 3, 2],
+			},
+		],
+	},
+	{
+		id: 3,
+		page: 3,
+		title: '20 Naira Note',
+		image: '',
+		text: 'The green-coloured paper banknote of Twenty Naira features the portrait of Army General Murtala R. Muhammed (1938-1976) in military uniform on the front side. The back side of the old ₦20 bill shows the coat of arms of Nigeria with the national motto “Unity and Faith”.',
+		exercise: [
+			{
+				id: 1,
+				question: 'Count and add up all the 20 naira note(s).',
+				type: '20naira',
+				objectives: [
+					{
+						answer: 3,
+						options: [2, 3, 1, 4],
+					},
+					{
+						answer: 2,
+						options: [5, 3, 6, 2],
+					},
+					{
+						answer: 4,
+						options: [2, 8, 6, 4],
+					},
+				],
+				// options: [1, 3, 2],
+			},
+		],
+	},
+	{
+		id: 4,
+		page: 4,
+		title: '50 Naira Note',
+		text: 'The blue-coloured, polymer banknote of 50 Naira features the People of Nigeria, showing four portraits of Nigerians. The ₦50 bill has a see-through window. On the back side of the Fifty Naira banknote is an image of three fishermen at work.',
+		image: '',
+		exercise: [
+			{
+				id: 1,
+				question: 'Count and add up all of the 50 naira note.',
+				type: '50naira',
+				objectives: [
+					{
+						answer: 3,
+						options: [2, 3, 1, 4],
+					},
+					{
+						answer: 4,
+						options: [4, 3, 6, 2],
+					},
+					{
+						answer: 1,
+						options: [2, 8, 6, 1],
+					},
+				],
+				// options: [1, 3, 2],
+			},
+		],
+	},
+	{
+		id: 5,
+		page: 5,
+		title: 'Drag and Drop',
+		text: 'Identify the Naira Notes below by dragging and dropping each note on the right place.',
+		image: '',
+		exercise: [
+			{
+				id: 1,
+				question: 'Drag coin from the left to its value on the right',
+				options: [
+					{ image: 'five.png', name: 'Five' },
+					{ image: 'ten.png', name: 'Ten' },
+					{ image: 'twenty.png', name: 'Twenty' },
+					{ image: 'fifty.png', name: 'Fifty' },
+				],
+			},
+		],
+	},
+	{
+		id: 6,
+		page: 6,
+		title: 'Drag and Drop',
+		text: 'Identify the Naira Notes below by dragging and dropping each note on the right place.',
+		image: '',
+		exercise: [
+			{
+				id: 1,
+				question: 'Drag coin from the left to its value on the right',
+				options: [
+					{ image: 'five.png', name: 'Five' },
+					{ image: 'ten.png', name: 'Ten' },
+					{ image: 'twenty.png', name: 'Twenty' },
+					{ image: 'fifty.png', name: 'Fifty' },
+				],
+			},
+		],
+	},
+	{
+		id: 7,
+		page: 7,
+		title: 'Drag and Drop',
+		text: 'Identify the Naira Notes below by dragging and dropping each note on the right place.',
+		image: '',
+		exercise: [
+			{
+				id: 1,
+				question: 'Drag coin from the left to its value on the right',
+				options: [
+					{ image: 'five.png', name: 'Five' },
+					{ image: 'ten.png', name: 'Ten' },
+					{ image: 'twenty.png', name: 'Twenty' },
+					{ image: 'fifty.png', name: 'Fifty' },
+				],
+			},
+		],
+	},
+	{
+		id: 8,
+		page: 8,
+		title: "Let's Shop",
+		text: 'pick the coins below to show how much you need to buy this little bunny.',
+		image: '',
+		exercise: [
+			{
+				id: 1,
+				options: [],
+				answer: '25 Naira',
+			},
+		],
+	},
+	{
+		id: 9,
+		page: 9,
+		title: 'Making Lemonade!',
+		text: 'Lulu wants a new game. It costs 250 Naira. To earn money Lulu has a lemonade stand. Each glass of lemonade costs 5 Naira. Tell how many glass lulu needs to sell to buy the game. (Hint: 250 divided by 5)',
+		image: '',
+		exercise: [
+			{
+				id: 1,
+				options: [],
+			},
+		],
+	},
+];
 
 export const chaptersList = [
-  { title: "Introduction", icon: "hourglass-start" },
-  { title: "Budgeting", icon: "list-alt" },
-  { title: "Savings", icon: "piggy-bank" },
-  { title: "Debt", icon: "money-check" },
-  { title: "Spending", icon: "money-bill-alt" },
-  { title: "Income", icon: "cash-register" },
-  { title: "Investing", icon: "house-user" },
-  { title: "Crypto", icon: "bitcoin" },
-  { title: "Conclusion", icon: "hourglass-end" },
-]
+	{ title: 'Introduction', icon: 'hourglass-start' },
+	{ title: 'Budgeting', icon: 'list-alt' },
+	{ title: 'Savings', icon: 'piggy-bank' },
+	{ title: 'Debt', icon: 'money-check' },
+	{ title: 'Spending', icon: 'money-bill-alt' },
+	{ title: 'Income', icon: 'cash-register' },
+	{ title: 'Investing', icon: 'house-user' },
+	{ title: 'Conclusion', icon: 'hourglass-end' },
+];
